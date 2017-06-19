ce1b1065744108e17ca52248b59585d1086e9220
Mihnea Lazar <lzr_mihnea@yahoo.com>
2017-06-19 13:29:43 +0300

    starting from scratch

--- zoo-app/src/main/java/ZooApp.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lazar on 15/06/2017.
- */
-public class ZooApp {public static void main(String[] args) {
-}
-}
--- zoo-integrationtests/test/java/ZooAppIntegrationTest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by lazar on 15/06/2017.
- */
-public class ZooAppIntegrationTest {
-}
