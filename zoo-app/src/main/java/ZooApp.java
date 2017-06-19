62d14c993860d22a0f9118dbce74938ab18d06c8
Mihnea Lazar <lzr_mihnea@yahoo.com>
2017-06-19 13:21:57 +0300

    initial commit with project structure

---
+++ zoo-app/src/main/java/ZooApp.java
@@ -0,0 +1,6 @@
+/**
+ * Created by lazar on 15/06/2017.
+ */
+public class ZooApp {public static void main(String[] args) {
+}
+}
---
+++ zoo-integrationtests/test/java/ZooAppIntegrationTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by lazar on 15/06/2017.
+ */
+public class ZooAppIntegrationTest {
+}
